fb2f6fb437a67c2b2654d755768c9e89b0f68b03
Sunny Jang <35230852+sunny-jang@users.noreply.github.com>
2023-02-01 18:28:47 +0900

    Update codility_tieRopes.js

--- codility_tieRopes.js
+++ codility_tieRopes.js
@@ -1,25 +1,17 @@
 function solution(K,A) {
-  A.sort((a,b)=>b-a);
   let cnt = 0;
   let sum = 0;
 
   for(let i=0; i<A.length; i++) {
-    if(A[i] >= K) {
-      cnt++;
-      continue;
-    }
     sum+=A[i];
-
     if(sum>=K) {
       cnt++;
       sum=0;
     };
   }
-  console.log(cnt)
 
   return cnt;
 }
-
 solution(1,[1]);
 // solution([1,2,3,4,1,1,3], 1);
 // solution([1,2,3,4,1,1,3], 2);
